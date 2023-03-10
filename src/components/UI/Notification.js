30234fa4d2c5f2730fc90fa963ec22fca1da01bb
Alexander Mikirtychev <alexsmik@yandex.ru>
2023-03-10 14:57:50 +0300

    upd errors

--- src/components/UI/Notification.js
+++ src/components/UI/Notification.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import classes from './Notification.module.css';
 
-const Notification = (props) => {
+const Notification = ({status, title, message}) => {
     let specialClasses = '';
-    if (props.status === 'error') {
+    if (status === 'error') {
         specialClasses = classes.error;
     }
-    if (props.status === 'success') {
+    if (status === 'success') {
         specialClasses = classes.success;
     }
     const cssClasses = `${classes.notification} ${specialClasses}`;
     return (
         <section>
-            <h2>{props.title}</h2>
-            <p>{props.message}</p>
+            <h2>{title}</h2>
+            <p>{message}</p>
         </section>
     );
 };
